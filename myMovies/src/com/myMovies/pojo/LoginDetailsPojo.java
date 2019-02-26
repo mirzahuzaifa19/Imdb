9ad5fe61b615a0740f0286fc5dab076ccb276eeb
mirbeg <mirbeg@DIN80045047.corp.capgemini.com>
2019-02-26 17:14:44 +0530

    creating conection,dao nad pojo classes

---
+++ myMovies/src/com/myMovies/pojo/LoginDetailsPojo.java
@@ -0,0 +1,31 @@
+package com.myMovies.pojo;
+
+public class LoginDetailsPojo {
+private String userName;
+private String password;
+public LoginDetailsPojo() {
+	
+}
+public LoginDetailsPojo(String userName, String password) {
+	super();
+	this.userName = userName;
+	this.password = password;
+}
+public String getUserName() {
+	return userName;
+}
+public void setUserName(String userName) {
+	this.userName = userName;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+@Override
+public String toString() {
+	return "LoginDetailsPojo [userName=" + userName + ", password=" + password + "]";
+}
+
+}
